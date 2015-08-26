44f20ce385c6afd861f3f2bd0aa505bba853f2b7
郭浩 <dime@danwi.me>
2015-08-26 13:34:20 +0800

    zxing--修改布局文件和入口

--- src/main/java/me/dimoge/TechnologyHaggis/MainActivity.java
+++ src/main/java/me/dimoge/TechnologyHaggis/MainActivity.java
@@ -1,16 +1,28 @@
 package me.dimoge.TechnologyHaggis;
 
 
+import android.app.Activity;
 import android.os.Bundle;
-import com.journeyapps.barcodescanner.CaptureActivity;
+import android.view.View;
+import android.widget.Button;
 
 
-public class MainActivity extends CaptureActivity {
+public class MainActivity extends Activity {
+
+    Button zxing_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_app_start);
+        setContentView(R.layout.activity_main);
+
+        zxing_btn = (Button) findViewById(R.id.zxing_btn);
+        zxing_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
 
---
+++ src/main/java/me/dimoge/TechnologyHaggis/ZXingActivity.java
@@ -0,0 +1,16 @@
+package me.dimoge.TechnologyHaggis;
+
+import android.os.Bundle;
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+/**
+ * Created by dime on 2015/8/26 0026.
+ */
+public class ZXingActivity extends CaptureActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
