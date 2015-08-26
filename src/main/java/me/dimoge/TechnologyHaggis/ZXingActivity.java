4219a16a979ef61ed814ed1501c6ffda7892b756
郭浩 <dime@danwi.me>
2015-08-26 14:30:33 +0800

    zxing--一些修改

--- src/main/java/me/dimoge/TechnologyHaggis/ZXingActivity.java
+++ src/main/java/me/dimoge/TechnologyHaggis/ZXingActivity.java
@@ -1,17 +1,31 @@
 package me.dimoge.TechnologyHaggis;
 
+import android.app.Activity;
 import android.os.Bundle;
-import com.journeyapps.barcodescanner.CaptureActivity;
+
+import android.view.Window;
+import android.view.WindowManager;
+
 
 /**
  * Created by dime on 2015/8/26 0026.
  */
-public class ZXingActivity extends CaptureActivity {
+public class ZXingActivity extends Activity {
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // 保持Activity处于唤醒状态
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_zxing);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 }
