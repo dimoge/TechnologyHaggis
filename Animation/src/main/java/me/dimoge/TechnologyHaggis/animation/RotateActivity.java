b5e0efc9d37cc410596517dbbf3020b20f3ce7c9
郭浩 <dime@danwi.me>
2015-09-22 17:39:48 +0800

    animation:  rotateAnimation未完成

--- Animation/src/main/java/me/dimoge/TechnologyHaggis/animation/RotateActivity.java
+++ Animation/src/main/java/me/dimoge/TechnologyHaggis/animation/RotateActivity.java
@@ -1,9 +1,62 @@
 package me.dimoge.TechnologyHaggis.animation;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.*;
+import android.widget.Button;
+import android.widget.ImageView;
 
 /**
+ * rotateAnimation 旋转动画
+ * 需要注意的是 旋转中心点, 旋转速度(加速, 减速, 匀速)
  * Created by dime on 2015/9/22 0022.
  */
 public class RotateActivity extends Activity {
+
+    ImageView img;
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rotate);
+
+        img = (ImageView) findViewById(R.id.img);
+        btn = (Button) findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /**RotateAnimation 构造方法 共6个参数
+                 * @param fromDegree 开始旋转角度 float[0f-360f]
+                 * @param toDegree 结束旋转角度 float[0f-360f]
+                 * @param X相对位置 Anmiation.RELATIVE_TO_SELF | Animation.RELATIVE_TO_PARENT
+                 * @param pivoteX 圆形X坐标 float[0f-1f]
+                 * @param Y相对位置 Anmiation.RELATIVE_TO_SELF | Animation.RELATIVE_TO_PARENT
+                 * @param pivoteY 圆形Y坐标 float[0f-1f]
+                 */
+                RotateAnimation rotateAnimation = new RotateAnimation(0f, 360f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);//0.5代表中心点
+                rotateAnimation.setDuration(3000);//详细可参见alphaAnimation
+                rotateAnimation.setRepeatMode(Animation.RESTART);//重新开始 同上
+                rotateAnimation.setRepeatCount(Animation.INFINITE);//无限重复 同上
+                rotateAnimation.setFillAfter(true);//停止, 处于动画结束的最后一帧
+                rotateAnimation.setFillBefore(true);//.....
+
+                /**设置速度
+                 * @param Interpolator 动画速率变化的控制
+                 * 五种速率控制, 说明如下
+                 */
+                LinearInterpolator li = new LinearInterpolator();//匀速
+                AccelerateDecelerateInterpolator adli = new AccelerateDecelerateInterpolator();//在动画结束和开始的地方速率改变比较慢, 在中间的时候加速
+                AccelerateInterpolator ali = new AccelerateInterpolator();//在动画开始的地方速率改变比较慢, 然后开始加速
+                CycleInterpolator cli = new CycleInterpolator(10);//动画循环特定的次数, 速率改变沿正玄曲线(需要指定动画循环次数)
+                DecelerateInterpolator dli = new DecelerateInterpolator();//在动画开始的地方速率改变比较慢, 然后开始减速
+                rotateAnimation.setInterpolator(li);
+
+                img.startAnimation(rotateAnimation);
+            }
+        });
+    }
 }
