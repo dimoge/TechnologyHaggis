961b678eb8d518613b3fa3176d7ce9206296dab3
郭浩 <dime@danwi.me>
2015-09-28 15:00:25 +0800

    HTTP:xutil框架学习

--- HTTP/src/main/java/me/dimoge/TechnologyHaggis/http/MainActivity.java
+++ HTTP/src/main/java/me/dimoge/TechnologyHaggis/http/MainActivity.java
@@ -1,26 +1,22 @@
 package me.dimoge.TechnologyHaggis.http;
 
 import android.app.Activity;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
+import com.lidroid.xutils.HttpUtils;
+import com.lidroid.xutils.exception.HttpException;
+import com.lidroid.xutils.http.RequestParams;
+import com.lidroid.xutils.http.ResponseInfo;
+import com.lidroid.xutils.http.callback.RequestCallBack;
+import com.lidroid.xutils.http.client.HttpRequest;
 
 public class MainActivity extends Activity {
 
     Button btn;
-    ImageView img;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,51 +24,55 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         btn = (Button) findViewById(R.id.btn);
-        img = (ImageView) findViewById(R.id.img);
+        textView = (TextView) findViewById(R.id.textView);
 
         btn.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                String urlPathContent = "http://s16.sinaimg.cn/orignal/89429f6dhb99b4903ebcf&690";
-                try {
-                    byte[] data = getImage(urlPathContent);
-                    Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);  //生成位图
-                    img.setImageBitmap(bitmap);   //显示图片
-                } catch (IOException e) {
-                    Toast.makeText(MainActivity.this, "c出错了啊", Toast.LENGTH_LONG).show();  //通知用户连接超时信息
-                    Log.i("xxxx", e.toString());
-                }
+                HttpUtils http = new HttpUtils();
+                RequestParams params = new RequestParams();
+                params.addBodyParameter("displayId", "0000-11111-2222-3333");
 
+                http.send(HttpRequest.HttpMethod.POST,   //@param 请求类型:POST
+                        "http://sbuu.danwi.me/app_api/display/getInfo.do",    //@param 请求地址 http://www.baidu.com.....
+                        params, //@param post请求参数
+                        new RequestCallBack<Object>() {     //@param 请求回调
+                            @Override
+                            public void onSuccess(ResponseInfo<Object> responseInfo) {
+                                Toast.makeText(MainActivity.this, "post请求成功", Toast.LENGTH_SHORT).show();
+                                textView.setText(responseInfo.result.toString());
+                            }
+
+                            @Override
+                            public void onFailure(HttpException e, String s) {
+                                Toast.makeText(MainActivity.this, "post请求失败!!!!1"+s, Toast.LENGTH_SHORT).show();
+                            }
+
+                            @Override
+                            public void onLoading(long total, long current, boolean isUploading) {
+                                super.onLoading(total, current, isUploading);
+                            }
+
+                            @Override
+                            public void onCancelled() {
+                                super.onCancelled();
+                            }
+
+                            @Override
+                            public void onStart() {
+                                super.onStart();
+                            }
+
+                            @Override
+                            protected Object clone() throws CloneNotSupportedException {
+                                return super.clone();
+                            }
+
+
+                        });
             }
         });
     }
-
-    public static byte[] getImage(String path) throws IOException {
-        URL url = new URL(path);
-        HttpURLConnection conn = (HttpURLConnection)url.openConnection();
-        conn.setRequestMethod("GET");   //设置请求方法为GET
-        conn.setReadTimeout(5*1000);    //设置请求过时时间为5秒
-        InputStream inputStream = conn.getInputStream();   //通过输入流获得图片数据
-        byte[] data = readInputStream(inputStream);     //获得图片的二进制数据
-        return data;
-
-    }
-
-    /*
-    * 从数据流中获得数据
-    */
-    public static  byte[] readInputStream(InputStream inputStream) throws IOException {
-        byte[] buffer = new byte[1024];
-        int len = 0;
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        while((len = inputStream.read(buffer)) != -1) {
-            bos.write(buffer, 0, len);
-        }
-        bos.close();
-        return bos.toByteArray();
-
-    }
-
 
 }
