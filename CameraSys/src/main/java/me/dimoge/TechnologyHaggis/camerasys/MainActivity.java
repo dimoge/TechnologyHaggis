7e0808dfb6a20d4456b90acbe335bdc9fcc9e986
郭浩 <dime@danwi.me>
2015-10-08 18:04:36 +0800

    CameraSys: 相册完成

--- CameraSys/src/main/java/me/dimoge/TechnologyHaggis/camerasys/MainActivity.java
+++ CameraSys/src/main/java/me/dimoge/TechnologyHaggis/camerasys/MainActivity.java
@@ -4,10 +4,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -16,7 +16,6 @@ import android.widget.Toast;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -25,6 +24,9 @@ public class MainActivity extends Activity {
 
     Button btn, btn2;
     ImageView img;
+
+    String BASEPAHT = "/sdcard/TechnologyHaggis/CameraSys/";
+    String dataStr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,10 +45,28 @@ public class MainActivity extends Activity {
                     Toast.makeText(MainActivity.this, "系统没有相机", Toast.LENGTH_SHORT).show();
                     return;
                 }else{
+//                    指定照片存储的地址
+//                    File file = new File("/sdcard/TechnologyHaggis/CameraSys/1.jgp");
+//                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//                    intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
                     startActivityForResult(new Intent(MediaStore.ACTION_IMAGE_CAPTURE), 1);
                 }
+            }
+        });
 
-
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("image/*");
+                intent.putExtra("crop", "true");
+                intent.putExtra("aspectX",1);
+                intent.putExtra("aspectY",1);
+                intent.putExtra("outputX", 80);
+                intent.putExtra("outputY", 80);
+                intent.putExtra("return-data",true);
+                startActivityForResult(intent, 11);
             }
         });
     }
@@ -61,15 +81,14 @@ public class MainActivity extends Activity {
             String sdStatus = Environment.getExternalStorageState();
             if(sdStatus.equals(Environment.MEDIA_MOUNTED)){//有sd卡
                 FileOutputStream b = null;
-                File file = new File("/sdcard/TechnologyHaggis/CameraSys/");
+                File file = new File(BASEPAHT);
                 file.mkdirs();// 创建文件夹，名称为myimage
 
-                String str=null;
                 Date date=null;
                 SimpleDateFormat format = new SimpleDateFormat("yyyyMMddHHmmss");//获取当前时间，进一步转化为字符串
                 date =new Date();
-                str=format.format(date);
-                String fileName = "/sdcard/TechnologyHaggis/CameraSys/"+str+".jpg";
+                dataStr=format.format(date);
+                String fileName = BASEPAHT+dataStr+".jpg";
                 try {
                     b = new FileOutputStream(fileName);
                     bitmap.compress(Bitmap.CompressFormat.JPEG, 100, b);// 把数据写入文件
@@ -83,6 +102,14 @@ public class MainActivity extends Activity {
                         e.printStackTrace();
                     }
                 }
+                //开始裁剪
+                Intent intentCat = new Intent("com.android.camera.action.CROP");
+                File f = new File(BASEPAHT+dataStr+".jpg");
+                intentCat.setDataAndType(Uri.fromFile(f), "image/*");
+                // aspectX aspectY 是宽高的比例
+                intentCat.putExtra("aspectX", 1);
+                intentCat.putExtra("aspectY", 1);
+                startActivityForResult(intentCat, 3);
             }else{
                 Toast.makeText(MainActivity.this, "没有可用的sd卡", Toast.LENGTH_SHORT).show();
             }
