3f405a2808be79a79684d6bfc1b8592f250a54c5
dime <dime@danwi.me>
2015-12-21 15:14:23 +0800

    完善viewpager

--- ViewPager-Study/src/main/java/me/danwi/dime/viewpager_study/app/MainActivity.java
+++ ViewPager-Study/src/main/java/me/danwi/dime/viewpager_study/app/MainActivity.java
@@ -4,9 +4,11 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import java.util.ArrayList;
@@ -26,55 +28,54 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        LinearLayout linearLayout = new LinearLayout(this);
-
-        bg.add(R.layout.item_one);
-        bg.add(R.layout.item_two);
-        bg.add(R.layout.item_three);
-        bg.add(R.layout.item_four);
-        bg.add(R.layout.item_five);
+        bg.add(R.mipmap.book_zoo);
+        bg.add(R.mipmap.button_back);
+        bg.add(R.mipmap.button_back_pressed);
+        bg.add(R.mipmap.button_pressed);
+        bg.add(R.mipmap.camera_button);
 
         viewPager = (ViewPager) findViewById(R.id.viewpager);
         for(int i=0; i<5 ; i++ ){
-            View view  = LayoutInflater.from(MainActivity.this).inflate(bg.get(i),null);
-            views.add(view);
+            ImageView imageView = new ImageView(MainActivity.this);
+            imageView.setBackgroundResource(bg.get(i));
+            views.add(imageView);
         }
         viewPager.setAdapter(pagerAdapter);
     }
 
     PagerAdapter pagerAdapter = new PagerAdapter() {
         @Override
-        public boolean isViewFromObject(View view, Object o) {
-            return view==o;
+        public int getCount() {
+            return views.size();
         }
 
         @Override
-        public int getCount() {
-            return -1;
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
         }
 
         @Override
         public void destroyItem(ViewGroup container, int position, Object object) {
 //            super.destroyItem(container, position, object);
-            container.removeView(views.get(position%5));
+            container.removeView(views.get(position));
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+//            return super.instantiateItem(container, position);
+            container.addView(views.get(position));
+            return views.get(position);
         }
 
         @Override
         public int getItemPosition(Object object) {
             return super.getItemPosition(object);
         }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-
-            return "title";
-        }
-
-        @Override
-        public Object instantiateItem(ViewGroup container, int position) {
-            container.addView(views.get(position%5));
-
-            return views.get(position%5);
-        }
     };
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e("onResume", "onnononono");
+    }
 }
